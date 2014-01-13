9d979f0a8903b410697f5eb118866116ab772569
Bryan Kyle <bryan.kyle@gmail.com>
2014-01-13 08:26:34 -0800

    Created install_key.sh

---
+++ install_key.sh
@@ -0,0 +1,3 @@
+cat <<EOF >> ~/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIBu22iSrIj11spRTeUZ/WlDdVvsePLumaQDGujAOhGweR4OtAQ8kyDB5uwQ6RJlVCnH2foUfJhfVriNPA3fLnaLujs7QtSu6AhGL/2/4VzihLl2rfX5YbtSIc3dgn+9tbmRQQv9exyS70RjKRD0sbTuohY5qMoqunX8cqhEua1kbQ==
+EOF
